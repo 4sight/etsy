01d2a6efbf1786969916dee6cb67bbd95b60a225
Greeley Dawson <hgd4th@gmail.com>
2014-06-17 18:30:25 -0400

    gh-pages commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 etsyitems.results.forEach(function(a){
 	$('.products').append(
-		'<div class="wrapper"><img src="' + a.Images[0].url_570xN + '" style="float: left;" height="200px" width ="250px"><div class="title">' + a.title + '$' + a.price + a.currency_code + '</div><div class="category">' + a.category_path[0] + '</div></div>'
+		'<div class="wrapper"><img src="' + a.Images[0].url_570xN + '" style="float: left;" height="200px" width ="250px"><div class="title">' + a.title + '$' + a.price + a.currency_code + '</div><div class="category">' + a.category_path[0] + '</div><div class="price">' + a.price + ' USD</div></div>'
 		);
 });
