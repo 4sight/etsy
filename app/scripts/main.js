6ea89c0b716ed64e2539751a6231643fe38ddc09
Greeley Dawson <hgd4th@gmail.com>
2014-06-17 16:30:46 -0400

    Pulled images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 var newItem = $('<div class="item"></div>');
 etsyitems.results.forEach(function(a){
 	$('body').append(
-		'<div class="item">$' + a.price + ' ' + a.title + '</div>'
+		'<div class="item"><img src="' + a.Images[0].url_570xN + '">' + a.title + '$' + a.price + a.currency_code + '</div>'
 		);
 });
