d1dda564255e99b0bb83725ab1e60da076b42ab8
Greeley Dawson <hgd4th@gmail.com>
2014-06-17 17:21:20 -0400

    Formatting divs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,5 @@
-var newItem = $('<div class="item"></div>');
 etsyitems.results.forEach(function(a){
-	$('body').append(
-		'<div class="item"><img src="' + a.Images[0].url_570xN + '">' + a.title + '$' + a.price + a.currency_code + '</div>'
+	$('.products').append(
+		'<div class="pic"><img src="' + a.Images[0].url_570xN + '" style="float: left;" height="200px" width ="250px"><div class="info">' + a.title + '$' + a.price + a.currency_code + '</div></div>'
 		);
 });
