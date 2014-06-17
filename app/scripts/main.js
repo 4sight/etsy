ca161eb9342cc7e33b8c259f40fd16f517f3af71
Greeley Dawson <hgd4th@gmail.com>
2014-06-17 17:41:39 -0400

    Sizing divs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 etsyitems.results.forEach(function(a){
 	$('.products').append(
-		'<div class="pic"><img src="' + a.Images[0].url_570xN + '" style="float: left;" height="200px" width ="250px"><div class="info">' + a.title + '$' + a.price + a.currency_code + '</div></div>'
+		'<div class="wrapper"><div class="pic"><img src="' + a.Images[0].url_570xN + '" style="float: left;" height="200px" width ="250px"><div class="info">' + a.title + '$' + a.price + a.currency_code + '</div></div></div>'
 		);
 });
